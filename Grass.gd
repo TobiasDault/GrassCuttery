26f73765cf6c3bcbbb6906b20c90dd2e25fc120b
Samuel A. Mikell <sbug1442@gmail.com>
2023-03-28 13:20:59 -0400

    Created scenes and template scripts

---
+++ Grass.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# Grass Script
+# Contains grass growth formula
+# On being cut it sends a signal to main to award coins.
+# Receives a signal from Cutters to be cut.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Main Script
+# 1. Game Loop
+# 2. Weather Conditions
+# 3. Growth Checks
+# 4. t
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
