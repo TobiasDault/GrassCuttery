b688ea594cb8db56142ddb5b6c4819fa563a8670
Brent Melby <brentmelby@gmail.com>
2023-04-07 22:06:13 -0400

    Main game logic (attempt)
    Attempted to develop main game logic. Currently, Start on the main menu brings you to the main game scene. Here, the grass will grow but you're not able to cut it yet.

--- README.md
+++
@@ -1,6 +0,0 @@
-# GrassCuttery
-An idle game based around the growing and cutting of grass.
-Tobias Dault
-Dylan Vance
-Samuel Mikell
-Brent Melby
--- Scenes/MainMenu/MenuButtons.gd
+++ Scenes/MainMenu/MenuButtons.gd
@@ -1,13 +1,15 @@
-extends Area2D
+extends Node2D
 
-# Cutter Script
-# Up Next: Automatic Cutters
-# We need a UI for a shop where you can buy more cutters
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
