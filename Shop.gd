e7a4aaf65779b4c0b2b7d8c105a1321dcb3b8fd7
Samuel A. Mikell <sbug1442@gmail.com>
2023-04-26 09:00:54 -0400

    Fixed typo: lable -> label

--- Shop.gd
+++ Shop.gd
@@ -3,8 +3,8 @@ extends Control
 onready var money_label = $MoneyLabel
 onready var fertilizer_label = $FertilizerCont/Level1
 onready var irrigation_label = $IrrigationCont/Level2
-onready var cost1_lable = $FertilizerCont/Cost1
-onready var cost2_lable = $IrrigationCont/Cost2
+onready var cost1_label = $FertilizerCont/Cost1
+onready var cost2_label = $IrrigationCont/Cost2
 onready var autoMulti_label = $AutoGrowCont/autoMultiLabel
 onready var memory = $"/root/Memory" # this variable allows access to "global variables" stored in memory.gd
 
@@ -13,10 +13,10 @@ func _ready(): # setting each label to its correct value according to what is st
 	money_label.text = "Money: " + str(memory.money)
 	
 	fertilizer_label.text = "Current Level: " + str(int(memory.fertilizer["level"]))
-	cost1_lable.text = "Cost: " + str(int(memory.fertilizer["cost"]))
+	cost1_label.text = "Cost: " + str(int(memory.fertilizer["cost"]))
 	
 	irrigation_label.text = "Current Level: " + str(int(memory.irrigation["level"]))
-	cost2_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
+	cost2_label.text = "Cost: " + str(int(memory.irrigation["cost"]))
 	
 	autoMulti_label.text = "Mutliplier: " + str(memory.autoMulti) + "x"
 
@@ -26,7 +26,7 @@ func _on_Fertilizer_pressed():
 		memory.fertilizer["level"] += 1
 		fertilizer_label.text = "Current Level: " + str(memory.fertilizer["level"])
 		memory.fertilizer["cost"] *= 1.05 # arbitrary multiplier for scaling upgrade prices
-		cost1_lable.text = "Cost: " + str(int(memory.fertilizer["cost"]))
+		cost1_label.text = "Cost: " + str(int(memory.fertilizer["cost"]))
 		money_label.text = "Money: " + str(memory.money)
 	
 	
@@ -36,7 +36,7 @@ func _on_Irrigation_pressed():
 		memory.irrigation["level"] += 1
 		irrigation_label.text = "Current Level: " + str(memory.irrigation["level"])
 		memory.irrigation["cost"] *= 1.05
-		cost2_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
+		cost2_label.text = "Cost: " + str(int(memory.irrigation["cost"]))
 		money_label.text = "Money: " + str(memory.money)
 
 func _on_BackButton_pressed():
