9f7dfc049af82184f884f8f97733fc40322f7b74
Tobias Dault <tchubs15@gmail.com>
2023-04-26 04:18:15 -0400

    buy buttons now work as intended, and update labels accordingly

--- Shop.gd
+++ Shop.gd
@@ -10,23 +10,30 @@ onready var memory = $"/root/Memory"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	money_label.text = "Money: " + str(memory.money)
-	fertilizer_label.text = "Current Level: " + str(memory.fertilizer["level"])
+	fertilizer_label.text = "Current Level: " + str(int(memory.fertilizer["level"]))
 	cost1_lable.text = "Cost: " + str(int(memory.fertilizer["cost"]))
-	irrigation_label.text = "Current Level: " + str(memory.irrigation["level"])
-	cost1_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
+	
+	irrigation_label.text = "Current Level: " + str(int(memory.irrigation["level"]))
+	cost2_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
 
 func _on_Fertilizer_pressed():
+	if(memory.money >= memory.fertilizer["cost"]):
+		memory.money -= memory.fertilizer["cost"]
+		#memory.fertilizer.erase("level")
+		#memory.fertilizer["level"] = temp + 1;
+		memory.fertilizer["level"] += 1
+		fertilizer_label.text = "Current Level: " + str(memory.fertilizer["level"])
+		memory.fertilizer["cost"] *= 1.05
+		cost1_lable.text = "Cost: " + str(int(memory.fertilizer["cost"]))
+		money_label.text = "Money: " + str(memory.money)
 	
-	memory.fertilizer["level"] += 1
-	fertilizer_label.text = "Current Level: " + str(memory.fertilizer["level"])
-	memory.fertilizer["cost"] *= 1.05
-	cost1_lable.text = "Cost: " + str(int(memory.fertilizer["cost"]))
 	
 func _on_Irrigation_pressed():
 	memory.irrigation["level"] += 1
 	irrigation_label.text = "Current Level: " + str(memory.irrigation["level"])
 	memory.irrigation["cost"] *= 1.05
-	cost1_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
+	cost2_lable.text = "Cost: " + str(int(memory.irrigation["cost"]))
+	money_label.text = "Money: " + str(memory.money)
 
 func _on_BackButton_pressed():
 # warning-ignore:return_value_discarded
