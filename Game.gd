b6db901b4f8e66a4d489229834c751dd50ad17bd
cutterjam <cutterjames108@gmail.com>
2023-04-25 20:40:24 -0400

    Added more grass. Clickable. Cuttable

--- Game.gd
+++ Game.gd
@@ -1,33 +1,29 @@
 extends Node2D
 
-onready var sprite = $GrassSprite
-onready var animation_player = $GrassSprite/AnimationPlayer
-onready var timer = $Timer
-onready var area2D = $DetectMouse
-
-var stage = 0
-var max_stage = 2  # Change this to the total number of stages you have
+onready var grass_sprites = get_tree().get_nodes_in_group("grass-sprites")
+var max_stage = 9
+var sprite_stages = {}
 
 func _ready():
-	sprite.connect("mouse_entered", self, "_on_sprite_mouse_entered")
-	timer.connect("timeout", self, "_on_timer_timeout")
-	timer.start()
+	grass_sprites = get_tree().get_nodes_in_group("grass-sprites")
+	for sprite in grass_sprites:
+		sprite_stages[sprite] = 0
+		
+	var CutButton = get_node("CutButton")
+	CutButton.connect("pressed", self, "_on_cut_button_pressed")
 
-func _on_sprite_mouse_entered():
-	stage = 0
-	update_sprite()
+		
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		for sprite in grass_sprites:
+			sprite_stages[sprite] += 1
+			if sprite_stages[sprite] > max_stage:
+				sprite_stages[sprite] = max_stage
+			sprite.frame = sprite_stages[sprite]
+			
 
-func _on_timer_timeout():
-	stage += 1
-	if stage > max_stage:
-		stage = max_stage
-		timer.stop()
-	update_sprite()
-
-func update_sprite():
-	sprite.frame = stage
-	for child in area2D.get_children():
-		if child is CollisionShape2D:
-			child.disabled = true
-			if child.name == "stage_" + str(stage + 1) + "_shape":
-				child.disabled = false
+func _on_cut_button_pressed():
+	# Reset the stage of each sprite to 0
+	for sprite in grass_sprites:
+		sprite_stages[sprite] = 0
+		sprite.frame = 0
